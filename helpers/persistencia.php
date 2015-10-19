7890567a62123462cfb6ce1be6866c5b62b03040
leopiccionia <leopiccionia@gmail.com>
2015-10-19 23:30:04 +0000

    Removal of deprecated functions.

--- helpers/persistencia.php
+++ helpers/persistencia.php
@@ -9,19 +9,19 @@
 	function loginPorEmail($email, $senha){
 		$conexao = mysqli_connect($db_servidor, $db_usuario, $db_senha);
 		
-		$query = mysql_query('SELECT nome FROM usuario WHERE email = "' . $email .'"');
+		$query = mysqli_query('SELECT nome FROM usuario WHERE email = "' . $email .'"');
 		$nome;
 		$id = -1; /*valor sentinela */
-		while($row = mysql_fetch_array($query)){
+		while($row = mysqli_fetch_array($query)){
 			$nome = $row['nome'];
 			$hash_senha = sha1($senha .$nome);
 		}
 	   
-		$query = mysql_query('SELECT * FROM usuario WHERE email = "' .$email .'" AND senha = "' .$senha .'"');
-		if($row = mysql_fetch_array($query))
+		$query = mysqli_query('SELECT * FROM usuario WHERE email = "' .$email .'" AND senha = "' .$senha .'"');
+		if($row = mysqli_fetch_array($query))
 			$id = $row['usuario_id'];
 		
-		$conexao->mysql_close();
+		mysqli_close($conexao);
 		if($id != -1)
 			return array('logado' => true, 'id' => $id);
 		return array('logado' => false);
@@ -31,12 +31,12 @@
 		$conexao = mysqli_connect($db_servidor, $db_usuario, $db_senha);
 		$hash_senha = sha1($senha .$nome);
 		
-		$query = mysql_query('SELECT * FROM usuario WHERE nome = "' . $nome . '" AND senha = "' .$senha .'"');
+		$query = mysqli_query('SELECT * FROM usuario WHERE nome = "' . $nome . '" AND senha = "' .$senha .'"');
 		$id = -1; /*valor sentinela */
-		if($row = mysql_fetch_array($query))
+		if($row = mysqli_fetch_array($query))
 			$id = $row['usuario_id'];
 
-		$conexao->mysql_close();
+		mysqli_close($conexao);
 		if($id != -1)
 			return array('logado' => true, 'id' => $id);
 		return array('logado' => false);
@@ -44,11 +44,11 @@
 	
 	function votacoesCriadas($id){
 		$conexao = mysqli_connect($db_servidor, $db_usuario, $db_senha);
-		$query = mysql_query('SELECT * FROM votacao WHERE autor_id = "' .$id .'"');
+		$query = mysqli_query('SELECT * FROM votacao WHERE autor_id = "' .$id .'"');
 		$resultado = array();
-		while($row = mysql_fetch_array())
+		while($row = mysqli_fetch_array())
 			array_push($resultado, $row);
-		$conexao->mysql_close();
+		mysqli_close($conexao);
 		return $resultado;
 	}
 
